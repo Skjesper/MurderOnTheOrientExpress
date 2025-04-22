0d247d55101cbc51f25b43310eb2711c4ba33bf5
Skjesper <jesperskeppstedt@gmail.com>
2025-04-22 09:27:12 +0200

    removed DS_Store

--- MurderOnTheOrientExpress/OrientExpress/obj/Debug/net8.0/OrientExpress.AssemblyInfo.cs
+++ MurderOnTheOrientExpress/OrientExpress/obj/Debug/net8.0/OrientExpress.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrientExpress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ae636e8e7649c0cd47d6ffc1228a8413f27631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0540691d32cba7106cbeb0e5e4e1f789bfef692b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrientExpress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrientExpress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
